cb0ccc3d9e045bddf0a85ec9ba13d6f7ae287074
Kayla <kaylaawallace7@gmail.com>
2018-03-29 17:48:30 -0400

    Routing

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor() { }
+    Messages: string[];
+
+  constructor() {
+    this.Messages = ['You can now choose your own pictures.'];
+   }
 
   ngOnInit() {
   }
