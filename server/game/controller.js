bb0a93219eb07c2766b26b01834c852b9ebaeddc
Kayla <kaylaawallace7@gmail.com>
2018-04-11 16:15:07 -0400

    Client & Server side

--- server/game/controller.js
+++ server/game/controller.js
@@ -7,5 +7,5 @@ var game = new Game();
 
 module.exports = app
     .get('/quotes', (req, res) => res.send(game.GetQuotes()))
-    .get('/state', (request, response) => response.send(game))
+    .get('/state', (req, res) => res.send(game))
     .post('/picture', (req, res) => res.send(game.FlipPicture()) )
